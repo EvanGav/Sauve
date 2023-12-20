aa230236ec8dac085e9f65559ab0b995e265def2
Gavrieli Evan <evan.gavrieli@etu.umontpellier.fr>
2023-12-21 00:29:50 +0100

    maj

--- Server/server.py
+++ Server/server.py
@@ -2,6 +2,8 @@ import socket
 import threading
 from Fichier import Fichier
 
+key="0qhau7ia8yi3d1tb07dq7xmrakpcaw9fefv6n3s0tmy117s4y8gk1n9cgnuqg5hbfppf6hjqnupxybpnrmneqbewijhdc8md5ihol8294f5q29kiea2uhgcpxqnylbe475876aqz8ki7ckx5x079z9s9w9ix1sqozjdivdfqe52kgdb3u7zze3ls9uwcea52bty0vgviufgtvpcxmivbk2l0bm84ujb2gmruoactm5a50znprye9to2zqcjc9ug4n044y9tyz2sj9xotjmssawn0x162ayc7w4c3xffdm13k3epv5zyok29c4pa77hcx"
+
 def recvall(sock, length):
     data = b''
     while len(data) < length:
@@ -11,36 +13,87 @@ def recvall(sock, length):
         data += packet
     return data
 
+def send_backup_file(file_to_send, socket):
+    print(f"Envoi du fichier : {file_to_send.get_path()}")
+    content = file_to_send.read()
+    content = xor_encrypt_decrypt(content)
+    file_to_send.set_path(file_to_send.get_path().replace("./", "./backup/"))
+    file_to_send.set_path(file_to_send.get_path().replace(".\\", ".\\backup\\"))
+    print(f"Nom du fichier nettoyé : {file_to_send.get_path()}")
+    # Créer un socket pour le client
+    file_name_length = len(file_to_send.get_path())
+    socket.send(file_name_length.to_bytes(4, byteorder='big'))
+    # Envoyer le nom du fichier
+    socket.send(file_to_send.get_path().encode())
+    print(f"Nom du fichier envoyé : {file_to_send.get_path()}")
+    # Lire et envoyer le contenu du fichier
+    file_content_length = len(content)
+    socket.send(file_content_length.to_bytes(4, byteorder='big'))
+    socket.send(content)
+    print(f"Le fichier {file_to_send.get_path()} a été envoyé.")
+
+def adjust_key_length(text_length):
+    key_length = len(key)
+    repetitions = text_length // key_length
+    remaining = text_length % key_length
+    adjusted_key = key * repetitions + key[:remaining]
+    return adjusted_key
+
+def xor_encrypt_decrypt(content):
+    key_bytes = bytes(adjust_key_length(len(content)), 'utf-8')
+    result_bytes = bytes([a ^ b for a, b in zip(content, key_bytes)])
+    return result_bytes
 
 # Fonction pour gérer les connexions clientes
 def handle_client(client_socket):
-    while True:
-        # Recevoir la longueur du nom de fichier
-        file_name_length = int.from_bytes(client_socket.recv(4), byteorder='big')
+    order = int.from_bytes(client_socket.recv(4), byteorder='big')
+    if order == 1:
+        while True:
+            # Recevoir la longueur du nom de fichier
+            file_name_length = int.from_bytes(client_socket.recv(4), byteorder='big')
 
-        if file_name_length == 0:
-            break  # Terminer la réception des fichiers
+            if file_name_length == 0:
+                break  # Terminer la réception des fichiers
 
-        # Recevoir le nom de fichier
-        file_name = client_socket.recv(file_name_length).decode()
+            # Recevoir le nom de fichier
+            file_name = client_socket.recv(file_name_length).decode()
 
-        print(f"Reception du fichier : {file_name}")
+            print(f"Reception du fichier : {file_name}")
 
-        # Recevoir la longueur du contenu du fichier
-        file_content_length = int.from_bytes(client_socket.recv(4), byteorder='big')
+            # Recevoir la longueur du contenu du fichier
+            file_content_length = int.from_bytes(client_socket.recv(4), byteorder='big')
 
-        # Recevoir le contenu du fichier
-        file_content = recvall(client_socket, file_content_length)
+            # Recevoir le contenu du fichier
+            file_content = recvall(client_socket, file_content_length)
 
-        # Créer une instance de la classe Fichier
-        received_file = Fichier(file_name)
-        received_file.content = file_content
+            file_content = xor_encrypt_decrypt(file_content)
 
-        # Vérifier et enregistrer le fichier sur le serveur
-        received_file.save_to_path()
-        print(f"Fichier {file_name} reçu et enregistré.")
+            # Créer une instance de la classe Fichier
+            received_file = Fichier(file_name)
+            received_file.content = file_content
+
+            # Vérifier et enregistrer le fichier sur le serveur
+            received_file.save_to_path()
+            print(f"Fichier {file_name} reçu et enregistré.")
+ 
+    elif order == 2:
+        while True:
+            print("Envoi d'un fichier")
+            file_name_length = int.from_bytes(client_socket.recv(4), byteorder='big')
+
+            if file_name_length == 0:
+                break  # Terminer la réception des fichiers
+    
+            # Recevoir le nom de fichier
+            file_name = client_socket.recv(file_name_length).decode()
+
+            print(f"Envoi du fichier : {file_name}")
+
+            send_backup_file(Fichier(file_name), client_socket)
 
     client_socket.close()
+
+
 
 # Adresse IP et port du serveur
 host = "127.0.0.1"
--- client.py
+++ client.py
@@ -7,6 +7,8 @@ from datetime import date
 import json
 
 
+
+
 config = configparser.ConfigParser()
 config.read('extensions.conf')
 allowed_extensions = config['Extensions']['allowed_extensions'].split(', ')
@@ -32,8 +34,23 @@ def clean_file_path(file_path):
     file_path = file_path.replace("/Users/"+os.getlogin()+"/","")
     file_path = file_path.replace("\\Users\\"+os.getlogin()+"\\","")
 
-    file_path = "./" + BASE_FOLDER +"/"+date.today().strftime("%d-%m-%Y")+"/" + file_path
+    file_path = os.path.join(".", BASE_FOLDER, date.today().strftime("%d-%m-%Y"), file_path)
     
+    return file_path
+
+def clean_file_path_for_retrieval(file_path,d):
+    # Supprimer les références relatives (../)
+    file_path = os.path.abspath(os.path.expanduser(file_path))
+
+    # Supprimer les chemins absolus (C:, D:, etc.)
+    if ':' in file_path:
+        drive, path = os.path.splitdrive(file_path)
+        if drive:
+            file_path = path
+    
+    file_path = file_path.replace("/Users/"+os.getlogin()+"/","")
+    file_path = file_path.replace("\\Users\\"+os.getlogin()+"\\","")
+    file_path = os.path.join(".", BASE_FOLDER, d, file_path)
     return file_path
 
 def load_logs():
@@ -94,7 +111,34 @@ def send_file(file_to_send, socket):
         socket.send(content)
 
         print(f"Le fichier {file_to_send.get_path()} a été envoyé.")
-    
+
+def recvall(sock, length):
+    data = b''
+    while len(data) < length:
+        packet = sock.recv(length - len(data))
+        if not packet:
+            return None
+        data += packet
+    return data
+
+
+def retrieve_data(socket):
+        # Recevoir la longueur du nom de fichier
+        file_name_length = int.from_bytes(socket.recv(4), byteorder='big')
+        # Recevoir le nom de fichier
+        file_name = socket.recv(file_name_length).decode()
+        print(f"Reception du fichier : {file_name}")
+        # Recevoir la longueur du contenu du fichier
+        file_content_length = int.from_bytes(socket.recv(4), byteorder='big')
+        # Recevoir le contenu du fichier
+        file_content = recvall(socket, file_content_length)
+        #file_content = decrypt_file(file_content, key)
+        # Créer une instance de la classe Fichier
+        received_file = Fichier(file_name)
+        received_file.content = file_content
+        # Vérifier et enregistrer le fichier sur le serveur
+        received_file.save_to_path()
+        print(f"Fichier {file_name} reçu et enregistré.")    
 
 def send_folder(folder_to_send, socket):
     for root, dirs, files in os.walk(folder_to_send.get_path()):
@@ -103,23 +147,99 @@ def send_folder(folder_to_send, socket):
         for dir_name in dirs:
             send_folder(Fichier(os.path.join(root, dir_name)), socket)
 
+
+def ask_retrieve_file(file_to_retrieve, socket):
+    
+    logs = load_logs()
+
+    print(f"Recherche du fichier {file_to_retrieve.get_path()} dans les logs...")
+    
+    if file_to_retrieve.get_path() in logs.keys():
+        # Le fichier existe dans les logs
+        print(f"Le fichier {file_to_retrieve.get_path()} existe.")
+
+        # Collecter les dates disponibles pour ce fichier
+        file_dates = [info["backup_date"] for path, info in logs.items() if path == file_to_retrieve.get_path()]
+
+        if len(set(file_dates)) > 1:
+            print("Ce fichier existe pour plusieurs dates. Voici les dates disponibles :")
+            for date in set(file_dates):
+                print(date)
+
+            chosen_date = input("Entrez la date pour laquelle récupérer le fichier : ")
+            if chosen_date in file_dates:
+                #print(f"Envoi du fichier {file_to_retrieve.get_path()} pour la date {chosen_date} au serveur.")
+                # Envoi au serveur du path du fichier à récupérer pour la date choisie
+                file_to_retrieve.set_path(clean_file_path_for_retrieval(file_to_retrieve.get_path(),chosen_date))
+                file_name_length = len(file_to_retrieve.get_path())
+                socket.send(file_name_length.to_bytes(4, byteorder='big'))
+                socket.send(file_to_retrieve.get_path().encode())
+                
+            else:
+                print("Date invalide.")
+        else:
+            # Il n'existe qu'une seule date pour ce fichier, donc on envoie directement au serveur
+            chosen_date = file_dates[0]
+            file_to_retrieve.set_path(clean_file_path_for_retrieval(file_to_retrieve.get_path(),chosen_date))
+            print(f"Nom du fichier nettoyé : {file_to_retrieve.get_path()}")
+            file_name_length = len(file_to_retrieve.get_path())
+            socket.send(file_name_length.to_bytes(4, byteorder='big'))
+            socket.send(file_to_retrieve.get_path().encode())
+        
+        retrieve_data(socket)
+    else:
+        print(f"Le fichier {file_to_retrieve.get_path()} n'existe pas dans les logs.")
+
+def ask_retrieve_folder(folder_to_retrieve, socket):
+    for root, dirs, files in os.walk(folder_to_retrieve.get_path()):
+        for file in files:
+            file_path = os.path.join(root, file)
+            ask_retrieve_file(Fichier(file_path), socket)
+    for dir_name in dirs:
+        ask_retrieve_folder(Fichier(os.path.join(root, dir_name)), socket)
+
+
+    
+
 # Adresse IP et port du serveur
 host = input("Entrez l'adresse IP du serveur : ")  # Mettez l'adresse IP du serveur ici
 port = int(input("Entrez le port du serveur : "))  # Mettez le port du serveur ici
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((host, port))
+#shared_key = perform_diffie_hellman_client(client)
 
+action=int(input("Entrez 1 pour envoyer un fichier, 2 pour récupérer un fichier : "))
+
+if action == 1:
+    client.send(int(1).to_bytes(4, byteorder='big'))
 # Création d'une instance de la classe Fichier avec le chemin du fichier à envoyer
-to_send = Fichier(input("entrez le path du fichier : "))  # Remplacez par le chemin de votre fichier
+    to_send = Fichier(input("entrez le path du fichier : "))  # Remplacez par le chemin de votre fichier
 
-if os.path.isfile(to_send.get_path()):
-    send_file(to_send, client)
-elif os.path.isdir(to_send.get_path()):
-    send_folder(to_send, client)
-else:
-    print("Le fichier ou dossier n'existe pas.")
+    if os.path.isfile(to_send.get_path()):
+        send_file(to_send, client)
+    elif os.path.isdir(to_send.get_path()):
+        send_folder(to_send, client)
+    else:
+        print("Le fichier ou dossier n'existe pas.")
 
-client.send(int(0).to_bytes(4, byteorder='big'))
+    client.send(int(0).to_bytes(4, byteorder='big'))
+
+elif action == 2:
+    
+    client.send(int(2).to_bytes(4, byteorder='big'))
+    to_retrieve = Fichier(input("entrez le path du fichier : "))
+    if os.name == 'nt':  # Si c'est Windows
+        to_retrieve.set_path(to_retrieve.get_path().replace("/", "\\"))
+
+    if os.path.isfile(to_retrieve.get_path()):
+        ask_retrieve_file(to_retrieve, client)
+    elif os.path.isdir(to_retrieve.get_path()):
+        ask_retrieve_folder(to_retrieve, client)
+    else:
+        print("Le fichier ou dossier n'existe pas.")
+    
+    client.send(int(0).to_bytes(4, byteorder='big'))
+
 
 client.close()
